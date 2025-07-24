b18881bc0a0db2605c864dfd8fed483a77927f4f
automatikerik <erikdaniel.rodriguez05@gmail.com>
2025-07-23 23:13:28 -0600

    Add files via upload
    Reto Modulo 1 Completado

---
+++ modulo1_reto.py
@@ -0,0 +1,11 @@
+nombre = "Erik"
+edad = 24
+altura = 1.60
+estudiante = True
+
+mayor_5_años = edad + 5
+mitad_edad = edad / 2
+
+print(f"Hola, soy {nombre}, tengo {edad} años, mido {altura}m.")
+print(f"\nEn 5 años tendre {mayor_5_años} años.")
+print(f"\nAhora la mitad de mi edad es {mitad_edad} años.")
